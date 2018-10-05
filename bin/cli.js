d13b79252be0ad5775b8738751ce87e278ca3bc3
wownmedia <mschot@wownmedia.com>
2018-10-05 11:40:58 +0000

    Feature: add register delegate CLI command

--- bin/cli.js
+++ bin/cli.js
@@ -10,10 +10,6 @@ commander
 commander
   .command('delegate <username>')
   .description('Register as delegate with <username>.')
-  .option('-n, --network <network>', 'Connect to network: [mainnet|devnet]', 'mainnet')
-  .option('-c, --node <node>', 'Connect directly to node <node>.')
-  .option('-f, --format <format>', 'Specify how to format the output [json|table]', 'json')
-  .option('-v, --verbose', 'Show verbose logging.')
   .option('-p, --passphrase [passphrase]', 'Enter your passphrase, if you do not enter e passphrase you will be prompted for it.')
   .option('-s, --signature [signature]', 'Enter your second signature, if you do not enter one you will be prompted for it.')
   .option('--fee [fee]', 'Specify a dynamic fee in Arktoshis. Defaults to 2500000000 = 25 ARK.')
@@ -23,10 +19,6 @@ commander
 commander
   .command('signature [signature]')
   .description('Create a second signature.')
-  .option('-n, --network <network>', 'Connect to network: [mainnet|devnet]', 'mainnet')
-  .option('-c, --node <node>', 'Connect directly to node <node>.')
-  .option('-f, --format <format>', 'Specify how to format the output [json|table]', 'json')
-  .option('-v, --verbose', 'Show verbose logging.')
   .option('-p, --passphrase [passphrase]', 'Enter your passphrase, if you do not enter e passphrase you will be prompted for it.')
   .option('--fee [fee]', 'Specify a dynamic fee in Arktoshis. Defaults to 500000000 = 5 ARK.')
   .option('-i, --interactive', 'Prompt to confirm transaction.')
