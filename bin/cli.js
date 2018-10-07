62344796a0b58efc68d177770e678189ebf2431c
wownmedia <mschot@wownmedia.com>
2018-10-07 10:37:26 +0000

    fix: check on redirected addresses for latest payout

--- bin/cli.js
+++ bin/cli.js
@@ -24,6 +24,16 @@ commander
   .option('-i, --interactive', 'Prompt to confirm transaction.')
   .action(async (signature, cmd) => wallet.signature(signature, cmd))
 
+commander
+  .command('send <amount> <recepient>')
+  .description('Send <amount> to <recepient>. Valid <amount> format examples: 10, USD10.4, EUR10000, 10000.4')
+  .option('-p, --passphrase [passphrase]', 'Enter your passphrase, if you do not enter e passphrase you will be prompted for it.')
+  .option('-s, --signature [signature]', 'Enter your second signature, if you do not enter one you will be prompted for it.')
+  .option('-b, --smartbridge [smartbridge]', 'Enter a string for the SmartBridge.')
+  .option('--fee [fee]', 'Specify a dynamic fee in Arktoshis. Defaults to 10000000 = 0.1 ARK.')
+  .option('-i, --interactive', 'Prompt to confirm transaction.')
+  .action(async (amount, recepient, cmd) => wallet.send(amount, recepient, cmd))
+
 if (!process.argv.slice(2).length) {
   commander.outputHelp()
 
