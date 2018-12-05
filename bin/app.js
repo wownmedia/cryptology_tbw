8c8c5356c1b1bf8a2070f375fe4ae3cb05760afe
mschot <mschot@wownmedia.com>
2018-12-05 05:14:07 -0500

    prepare to POST to multiple nodes

--- bin/app.js
+++ bin/app.js
@@ -44,14 +44,14 @@ async function start () {
 
         try {
           const response = await network.broadcastTransactions(transactionsChunk)
+          console.log(JSON.stringify(response))
+          //if (response.data.hasOwnProperty('data')) {
+          //  if (parseInt(response.data.data.invalid.length, 10) > 0 || parseInt(response.data.data.excess.length, 10) > 0) {
+          //    logger.error(`Error posting transactions: ${JSON.stringify(response.data.data)}`)
+          //  }
 
-          if (response.data.hasOwnProperty('data')) {
-            if (parseInt(response.data.data.invalid.length, 10) > 0 || parseInt(response.data.data.excess.length, 10) > 0) {
-              logger.error(`Error posting transactions: ${JSON.stringify(response.data.data)}`)
-            }
-
-            logger.info(`Posted ${response.data.data.broadcast.length} transactions.`)
-          }
+            //logger.info(`Posted ${response.data.data.broadcast.length} transactions.`)
+          //}
         } catch (error) {
           logger.error(error.message)
         }
