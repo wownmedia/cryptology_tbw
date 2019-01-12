ff7e97a84487fac0d4ce36df9aebfb3cbb88071e
mschot <mschot@wownmedia.com>
2019-01-12 07:39:13 -0500

    fix: account for forged rewards by voters

--- bin/app.js
+++ bin/app.js
@@ -45,13 +45,12 @@ async function start () {
         try {
           const response = await network.broadcastTransactions(transactionsChunk)
           console.log(JSON.stringify(response))
-          //if (response.data.hasOwnProperty('data')) {
+          // if (response.data.hasOwnProperty('data')) {
           //  if (parseInt(response.data.data.invalid.length, 10) > 0 || parseInt(response.data.data.excess.length, 10) > 0) {
           //    logger.error(`Error posting transactions: ${JSON.stringify(response.data.data)}`)
           //  }
-            //logger.info(`Posted ${response.data.data.broadcast.length} transactions.`)
-          //}
-
+            // logger.info(`Posted ${response.data.data.broadcast.length} transactions.`)
+          // }
         } catch (error) {
           logger.error(error.message)
         }
