7edc8a8f5065f1ce0813a93847045076eb673327
Marc Schot <marc@cryptology.hk>
2019-02-05 10:53:06 -0500

    V2 develop (#40)
    * Improved TBW calculation
    * Added optional pool hopping prevention

--- bin/app.js
+++ bin/app.js
@@ -6,34 +6,34 @@ const payoutBuilder = require('../lib/utils/payouts')
 const network = require('../lib/services/network')
 const logger = require('../lib/services/logger')
 const BigNumber = require('bignumber.js')
-BigNumber.config({ROUNDING_MODE: BigNumber.ROUND_DOWN})
+BigNumber.config({DECIMAL_PLACES: 8, ROUNDING_MODE: BigNumber.ROUND_DOWN})
 
 const ARKTOSHI = new BigNumber(Math.pow(10, 8))
-const FEES = new BigNumber(0.1).times(ARKTOSHI)
+const FEES = process.env.FEE ? new BigNumber(process.env.FEE).times(ARKTOSHI) : new BigNumber(0.1).times(ARKTOSHI)
 const MAX_TRANSACTIONS_PER_REQUEST = process.env.MAX_TRANSACTIONS_PER_REQUEST ? parseInt(process.env.MAX_TRANSACTIONS_PER_REQUEST, 10) : 40
 
 async function start () {
   try {
     const trueblockweight = new TrueBlockWeight()
-    const {payouts, delegateProfit, acfDonation} = await trueblockweight.generatePayouts()
+    const {payouts, delegateProfit, acfDonation, timestamp} = await trueblockweight.generatePayouts()
 
-    let {totalAmount, totalFees, transactions} = payoutBuilder.generatePayouts(payouts)
+    let {totalAmount, totalFees, transactions} = payoutBuilder.generatePayouts(payouts, timestamp)
 
-    const adminTransactions = payoutBuilder.generateAdminPayouts(delegateProfit)
+    const adminTransactions = payoutBuilder.generateAdminPayouts(delegateProfit, timestamp)
     if (adminTransactions.length) {
       totalAmount = totalAmount.plus(delegateProfit.toFixed(0))
       totalFees = totalFees.plus(FEES.times(adminTransactions.length))
     }
 
     if (acfDonation.gt(0)) {
-      const acfTransaction = payoutBuilder.generateAcfPayout(acfDonation)
+      const acfTransaction = payoutBuilder.generateAcfPayout(acfDonation, timestamp)
       totalAmount = totalAmount.plus(acfDonation.toFixed(0))
       totalFees = totalFees.plus(FEES)
       adminTransactions.push(acfTransaction)
     }
 
     logger.info('==================================================================================')
-    logger.info(`Ready to Payout: ${totalAmount.div(ARKTOSHI).toFixed(8)} + ${totalFees.div(ARKTOSHI).toFixed(1)} fees.`)
+    logger.info(`Ready to Payout: ${totalAmount.div(ARKTOSHI).toFixed(8)} + ${totalFees.div(ARKTOSHI).toFixed(8)} fees.`)
     logger.info('==================================================================================')
     const args = process.argv.slice(2)
     if (args.length >= 1 && args[0] === 'payout') {
@@ -45,13 +45,6 @@ async function start () {
         try {
           const response = await network.broadcastTransactions(transactionsChunk)
           console.log(JSON.stringify(response))
-          //if (response.data.hasOwnProperty('data')) {
-          //  if (parseInt(response.data.data.invalid.length, 10) > 0 || parseInt(response.data.data.excess.length, 10) > 0) {
-          //    logger.error(`Error posting transactions: ${JSON.stringify(response.data.data)}`)
-          //  }
-            //logger.info(`Posted ${response.data.data.broadcast.length} transactions.`)
-          //}
-
         } catch (error) {
           logger.error(error.message)
         }
