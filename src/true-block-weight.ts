1d86388ff27eca6b4c32c3c6126c6ac4837aff00
Marc Schot <info@cryptology.hk>
2019-11-21 15:28:11 -0500

    initial conversion to TypeScript

--- src/true-block-weight.ts
+++ src/true-block-weight.ts
@@ -4,29 +4,18 @@ import { TrueBlockWeightEngine } from "./utils";
 
 export class TrueBlockWeight {
 
-    private readonly trueBlockWeightEngine: TrueBlockWeightEngine;
-    private payouts: Payouts;
-
-    constructor() {
-        try {
-          this.trueBlockWeightEngine = new TrueBlockWeightEngine();
-        } catch (e) {
-            logger.error(e.message);
-            process.exit(1);
-        }
-    }
-
-    public async calculate() {
-        this.payouts = await this.trueBlockWeightEngine.generatePayouts();
+    public async calculate(): Promise<Payouts> {
+        const trueBlockWeightEngine = new TrueBlockWeightEngine();
+        return await trueBlockWeightEngine.generatePayouts();
     }
 
     public async payout() {
-        await this.calculate();
+        const payouts: Payouts = await this.calculate();
         // todo payout
     }
 
     public async check() {
-        await this.calculate();
+        const payouts: Payouts = await this.calculate();
         // todo show transactions
     }
 }
