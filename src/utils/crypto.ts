8edcbfb8cb43758addd945ac15296aa2c62c8288
Marc Schot <info@cryptology.hk>
2019-11-29 16:29:11 -0500

    fix:version 2 in deserializer

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,9 +1,11 @@
-import { Identities, Interfaces, Transactions } from "@arkecosystem/crypto";
+import { Identities, Interfaces, Transactions, Managers } from "@arkecosystem/crypto";
 // const arkUtils = arkecosystem.utils;
 // const ECPair = arkecosystem.ECPair;
 // const ECSignature = arkecosystem.ECSignature;
 // const arkCrypto = arkecosystem.crypto;
 // const transactionBuilder = arkecosystem.transactionBuilder;
+// todo WTF figure out how to generalize this
+Managers.configManager.setHeight(4006000);
 
 export class Crypto {
   public static getAddress(passphrase, networkVersion) {
