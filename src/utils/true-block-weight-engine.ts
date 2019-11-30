c4d443d2e79bec6014bd403a67f53124b1be117c
Marc Schot <info@cryptology.hk>
2019-11-29 19:58:17 -0500

    update: multipayments

--- src/utils/true-block-weight-engine.ts
+++ src/utils/true-block-weight-engine.ts
@@ -309,19 +309,19 @@ export class TrueBlockWeightEngine {
     for ( let transaction of delegatePayoutTransactions ) {
       if(transaction.recipientId !== null) {
         const height: BigNumber = new BigNumber(latestPayouts.get(transaction.recipientId));
-        //if(!height || height < transaction.height) {
+        if(height.isNaN() || height.lt(new BigNumber(transaction.height))) {
           logger.warn(`New height for ${transaction.recipientId}: from ${height} to ${transaction.height}`);
           latestPayouts.set(transaction.recipientId, transaction.height);
           latestPayoutsTimeStamp.set(transaction.recipientId, transaction.timestamp);
-        //}
+        }
       } else {
         for(let receiver of transaction.multiPayment) {
           const height: BigNumber = new BigNumber(latestPayouts.get(receiver.recipientId));
-          //if(!height || height < transaction.height) {
+          if(height.isNaN() || height.lt(new BigNumber(transaction.height))) {
             logger.warn(`New height for ${receiver.recipientId}: from ${height} to ${transaction.height}`);
             latestPayouts.set(receiver.recipientId, transaction.height);
             latestPayoutsTimeStamp.set(receiver.recipientId, transaction.timestamp);
-          //}
+          }
         }
       }
     }
