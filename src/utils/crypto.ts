a1e502c3279afb92d959f9d45d9fb677dbffecfb
Marc Schot <info@cryptology.hk>
2019-11-30 09:28:20 -0500

    update: JSDOC

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -7,9 +7,11 @@ import {
 
 export class Crypto {
     /**
-     *
-     * @param publicKey
-     * @param networkVersion
+     * Generate an Address from a public key for a blockchain.
+     * @param {string} publicKey The public key of the wallet to generate the address for.
+     * @param {number} networkVersion The network version of the blockchain to generate the address for.
+     * @returns {string} The generated address.
+     * @static
      */
     public static getAddressFromPublicKey(
         publicKey: string,
@@ -19,9 +21,11 @@ export class Crypto {
     }
 
     /**
-     *
-     * @param serialized
-     * @param blockHeight
+     * Deserialize a transaction
+     * @param {string} serialized The serialized transaction
+     * @param {number} blockHeight The height of the blockchain, this is used to determine if version 2 is enabled.
+     * @returns The transaction object.
+     * @static
      */
     public static deserializeTransaction(
         serialized: string,
