0bfa289581f598f6c01fdc340a7bb5c517a68fde
Marc Schot <info@cryptology.hk>
2019-12-11 15:38:47 -0500

    update: business nonce

--- src/utils/transaction-engine.ts
+++ src/utils/transaction-engine.ts
@@ -57,7 +57,7 @@ export class TransactionEngine {
                 i + this.config.transactionsPerMultitransfer
             );
 
-            if(chunk.length === 1) {
+            if(chunk.length === 2) {
                 const receiver: Receiver = {
                     wallet: chunk[0].wallet,
                     amount: chunk[0].amount,
