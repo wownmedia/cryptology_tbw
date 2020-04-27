54a214dc3e5b4ee88f170d5d036160c75e383826
Marc Schot <info@cryptology.hk>
2020-04-27 13:14:57 -0500

    debug: votermutations log

--- src/utils/true-block-weight-engine.ts
+++ src/utils/true-block-weight-engine.ts
@@ -268,7 +268,7 @@ export class TrueBlockWeightEngine {
                     votersRound.slice(0)
                 );
                 if(votersRound.indexOf("Aao3G3v97FZzdKFW27ntQJAjCkJhpWgdfz") > 0) {
-                    logger.debug(`calculatedVotersPerForgedBlock: ${votersRound.length} : ${height}`);
+                    logger.warn(`calculatedVotersPerForgedBlock: ${votersRound.length} : ${height}`);
                 }
 
             }
