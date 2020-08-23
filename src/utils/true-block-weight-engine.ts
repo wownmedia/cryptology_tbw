04810c4973b3fb145ff1d7714977c3f352802e87
Marc Schot <info@cryptology.hk>
2020-08-23 10:20:16 -0500

    debug: item.multipayment

--- src/utils/true-block-weight-engine.ts
+++ src/utils/true-block-weight-engine.ts
@@ -606,6 +606,7 @@ export class TrueBlockWeightEngine {
             let amount: BigNumber = item.amount;
             const fee: BigNumber = item.fee;
 
+            logger.info(`voter balances: ${JSON.stringify(item.multiPayment)}`);
             if (item.multiPayment !== null) {
                 for (const transaction of item.multiPayment) {
                     const transactionAmount: BigNumber = new BigNumber(
