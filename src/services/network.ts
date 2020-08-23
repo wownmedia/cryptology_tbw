fd8a1f4456b762749eb618e9e8b699cae285f4ac
Marc Schot <info@cryptology.hk>
2020-08-23 16:37:26 -0500

    update: stake logic

--- src/services/network.ts
+++ src/services/network.ts
@@ -181,7 +181,7 @@ export class Network {
             logger.info(`Stakes found for ${voter.address}`);
             const voterStakes = voter.stakes;
            for(const item in voterStakes) {
-                logger.info(`Stake: ${JSON.stringify(item)}`)
+                logger.info(`Stake: ${JSON.stringify(voterStakes[item])}`)
             }
 
         }
