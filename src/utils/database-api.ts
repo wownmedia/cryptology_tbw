4399728a2370d59a8ae8a2eea66633278a547ea1
Marc Schot <info@cryptology.hk>
2020-08-24 16:48:40 -0500

    debug: transactions

--- src/utils/database-api.ts
+++ src/utils/database-api.ts
@@ -340,7 +340,7 @@ export class DatabaseAPI {
 
                     //todo remove
                     if(senderId === "cmcsmGe18ngpEo35oGCdBKJ2ziguQSWNYG" || transaction.recipientId === "cmcsmGe18ngpEo35oGCdBKJ2ziguQSWNYG") {
-                        logger.info(`transaction: ${JSON.stringify(data.data)}`)
+                        logger.info(`transaction: ${JSON.stringify(data)}`)
                     }
 
                     return {
