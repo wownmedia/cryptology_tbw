56b5332fd0f20ff46bbe42f6fca09b19c9526e80
Marc Schot <info@cryptology.hk>
2020-08-25 13:56:42 -0500

    update: get epoch from networkConfig

--- src/utils/true-block-weight-engine.ts
+++ src/utils/true-block-weight-engine.ts
@@ -92,6 +92,7 @@ export class TrueBlockWeightEngine {
         try {
             this.networkConfig = await this.network.getNetworkConfig();
             this.epochTimestamp = TrueBlockWeightEngine.calculateTimestamp(this.networkConfig.milestones[0].block.epoch)
+            logger.info(`Epoch: ${this.epochTimestamp}`); //todo
             const delegatePublicKey: string = await this.network.getDelegatePublicKey(
                 this.config.delegate
             );
